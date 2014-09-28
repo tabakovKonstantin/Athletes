11df97497681511f7e2aea7dad25356af1e5d36c
unknown <konkov90@gmail.com>
2014-09-28 17:58:03 +0400

    Объеденение всех данных

--- R/main.R
+++ R/main.R
@@ -17,3 +17,26 @@ names(dataset) = as.vector(data$V1)[2 : length(data_transpose_cat)]
 dataset[1 : nrow(dataset), target :=  target_name]
 
 write.csv2(dataset, file = result_dataset_name, row.names = FALSE)
+
+View(dataset)
+
+#########################
+data_Endurance_1 = data.table( read.csv2("DataSet Endurance 1.csv"))
+data_Endurance_2 = data.table( read.csv2("E:/Work/R/Athletes/output_data/DataSet Endurance 2.csv"))
+
+data_Endurance_2_list = as.list.data.frame(data_Endurance_2)
+View(rbind(data_Endurance_1, data_Endurance_2_list))
+
+Full_dataset = data_Endurance_2
+
+names_file_list = list.files(path = path_dataset_files, pattern = "^DataSet")
+
+for(name_file in names_file_list) {
+  print(name_file)
+  tmp_dataset = data.table( read.csv2(paste(path_dataset_files, name_file, sep = "/")))
+  #View(tmp_dataset)
+  tmp_dataset_list = as.list.data.frame(tmp_dataset)
+  Full_dataset = rbind(Full_dataset, tmp_dataset_list)
+  
+}
+####################
