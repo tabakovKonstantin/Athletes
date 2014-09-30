7d4e58dd5e262756d543dfa346f5ebebb8d96dd2
unknown <konkov90@gmail.com>
2014-09-30 16:43:55 +0400

    Приведение таблицы к числовым значениям. И построение модели работает.

--- R/main.R
+++ R/main.R
@@ -24,3 +24,43 @@ data_preparation()
 full_data = row_bind()
 View(full_data)
 write.csv2(full_data, paste(path_output_data, "Full Dataset.csv", sep = "/"), row.names = FALSE)
+
+#########################
+full_data = data.table(read.csv2(paste(path_output_data, "Full Dataset.csv", sep = "/")))
+
+full_data_list = as.list.data.frame(full_data)
+
+for(i in 2 : (length(full_data_list)- 1)  ) {    ## исправить на один когда будут имена обьектов
+  tmp_column = full_data_list[[i]]
+  tmp_column_numeric = as.double(as.vector(tmp_column))
+  full_data_list[[i]] = tmp_column_numeric
+}
+
+full_data_table = do.call(data.table, full_data_list)
+
+#######################################
+size_train_data = round(nrow(full_data) * 0.4)
+
+train_data_row = sample(1 : nrow(full_data), size_train_data) 
+testing_data_row = setdiff(1:nrow(full_data), train_data_row)
+
+train_data = full_data[train_data_row,]
+testing_data = full_data[testing_data_row,]
+
+modelRF = randomForest(target ~ . - Name., full_data_table, na.action = na.omit)
+
+
+# full_data_list = as.list.data.frame(full_data)
+# 
+# for(i in 2 : (length(full_data_list)- 1)  ) {    ## исправить на один когда будут имена обьектов
+#   tmp_column = full_data_list[[i]]
+#   tmp_column_numeric = as.double(as.vector(tmp_column))
+#   full_data_list[[i]] = tmp_column_numeric
+# }
+# 
+# full_data_table = do.call(data.table, full_data_list)
+
+
+
+  
+  
